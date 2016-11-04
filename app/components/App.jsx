81f1bfd93dee1f8b3bacc3c29e47165b17ab8294
Roger Xu <roger.xu.2007@gmail.com>
2016-11-04 16:11:57 +0800

    Add drag support for Note component

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
 import uuid from 'uuid';
+
+import { compose } from 'redux';
+import { DragDropContext } from 'react-dnd';
+import HTML5Backend from 'react-dnd-html5-backend';
+
 import connect from '../libs/connect';
 import Lanes from './Lanes';
 import LaneActions from '../actions/LaneActions';
@@ -24,8 +29,7 @@ class App extends React.Component {
   }
 }
 
-// connect(state, actions)
-export default connect(
-  ({ lanes }) => ({ lanes }),
-  { LaneActions }
+export default compose(
+  DragDropContext(HTML5Backend),
+  connect(({ lanes }) => ({ lanes }), { LaneActions })
 )(App);
--- app/components/Note.jsx
+++ app/components/Note.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import { DragSource } from 'react-dnd';
+import ItemTypes from '../constants/itemTypes';
 
-export default function Note({ children, ...props }) {
-  return (
+const Note = ({ connectDragSource, children, ...props }) => {
+  return connectDragSource(
     <div {...props}>
       {children}
     </div>
   );
-}
+};
+
+const noteSource = {
+  beginDrag(props) {
+    console.log('begin dragging note', props);
+
+    return {};
+  }
+};
+
+export default DragSource(ItemTypes.NOTE, noteSource, connect => ({
+  connectDragSource: connect.dragSource()
+}))(Note);
